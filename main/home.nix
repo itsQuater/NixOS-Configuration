a46c5444d2990587e9ad4715b3791ec2adf08799
Luna <luigiq99@gmail.com>
2025-07-11 17:11:34 +0200

    NixOS Configuration

--- main/home.nix
+++ main/home.nix
@@ -4,17 +4,20 @@
   home.username = "luna";
   home.homeDirectory = "/home/luna";
 
-  home.stateVersion = "25.05"; # Please read the comment before changing.
+  home.stateVersion = "25.05";#Imports and configurates the fastfetch package. 
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
+{config, pkgs, lib, ...}:
+
+{
+	environment.systemPackages = [
+		pkgs.fastfetch
+	];
+}
   
   home.packages = [	
     pkgs.hello
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
 		
   };
@@ -23,7 +26,5 @@
     EDITOR = "neovim";
   };
   
-
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
