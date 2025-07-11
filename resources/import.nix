89f857179d2b37b9f8d1fda0018a99723094f3e8
Luna <luigiq99@gmail.com>
2025-07-11 16:52:03 +0200

    NixOS Configuration

--- resources/import.nix
+++ resources/import.nix
@@ -6,20 +6,20 @@
    imports = [
 
       #Packages imports:
-      ./nix-os/packages/fastfetch.nix
-      ./nix-os/packages/firefox.nix
-      ./nix-os/packages/git.nix
-      ./nix-os/packages/davinciResolve.nix
-      ./nix-os/packages/discord.nix
-      ./nix-os/packages/libreOffice.nix
-      ./nix-os/packages/obs.nix
-      ./nix-os/packages/spotify.nix
-      ./nix-os/packages/steam.nix
-      ./nix-os/packages/vlc.nix
-      ./nix-os/packages/thunderbird.nix
-      ./nix-os/packages/minecraft.nix
+      ./packages/fastfetch.nix
+      ./packages/firefox.nix
+      ./packages/git.nix
+      ./packages/davinciResolve.nix
+      ./packages/discord.nix
+      ./packages/libreOffice.nix
+      ./packages/obs.nix
+      ./packages/spotify.nix
+      ./packages/steam.nix
+      ./packages/vlc.nix
+      ./packages/thunderbird.nix
+      ./packages/minecraft.nix
 
       #System imports:
-      ./nix-os/system/importsystem.nix
+      ./system/importsystem.nix
    ];
 }
