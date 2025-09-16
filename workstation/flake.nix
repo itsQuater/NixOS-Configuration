ac9b2d939d1f413c95aa416a36880600204b8f61
Luna <luigiq99@gmail.com>
2025-09-17 00:15:48 +0200

    Create new configuration

---
+++ workstation/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+    	url = "github:nix-community/home-manager";
+    	inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./core/configuration.nix
+        inputs.home-manager.nixosModules.home-manager {
+		home-manager.useGlobalPkgs = true;
+		home-manager.useUserPackages = true;
+		home-manager.extraSpecialArgs = {inherit inputs;};
+	}
+      ];
+    };
+  };
+}
