1259b0ab5ebb7c23313b2b7979db69aa293bb503
Luna <luigiq99@gmail.com>
2025-09-26 17:18:42 +0200

    Add flatpak support

--- workstation/sys/system.nix
+++ workstation/sys/system.nix
@@ -56,6 +56,18 @@
    };
    # ---
 
+   # Add flatpak support.
+   # ---
+   services.flatpak.enable = true;
+   systemd.services.flatpak-repo = {
+      wantedBy = [ "multi-user.target" ];
+      path = [ pkgs.flatpak ];
+      script = ''
+         flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+      '';
+   };
+   # ---
+
 
    # Video drivers & codecs and accelerated video playback.
    # ---
