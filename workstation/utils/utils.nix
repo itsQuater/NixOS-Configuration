e50415ab8f36d15751eca5097db75e3f5757328d
Luna <luigiq99@gmail.com>
2025-09-26 17:13:08 +0200

    Move part of configuration into launcher.nix

--- workstation/utils/utils.nix
+++ workstation/utils/utils.nix
@@ -52,29 +52,7 @@
    };
    # ---
 
-   # Add an anime-game launcher.
-   # ---
-   let
-      aagl = import (builtins.fetchTarball "https://github.com/ezKEa/aagl-gtk-on-nix/archive/release-25.05.tar.gz");
-   in
-   {
-     imports = [
-       aagl.module
-     ];
- 
-      nix.settings = aagl.nixConfig;
-      programs = {
-         #anime-game-launcher.enable = true;
-         #anime-games-launcher.enable = true;
-         honkers-railway-launcher.enable = true;
-         #honkers-launcher.enable = true;
-         #wavey-launcher.enable = true;
-         #sleepy-launcher.enable = true;
-      };
-   };
-   # ---
-
-
+   
    # Install other applications.
    # ---
    imports = [
