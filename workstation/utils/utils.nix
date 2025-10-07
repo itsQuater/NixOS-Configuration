4952b8ccf3fb6f2ff55b98fdad7a5a5d281f25e6
Kiana <luigiq99@gmail.com>
2025-10-07 17:11:09 +0200

    Configure firefox

--- workstation/utils/utils.nix
+++ workstation/utils/utils.nix
@@ -4,7 +4,18 @@
    # Install Firefox browser.
    # ---
    programs = {
-      firefox.enable = true;
+      firefox = {
+         enable = true;
+         languagePacks = ["en-US" "pl"];
+         DisplayBookmarksToolbar = "always';
+         ExtensionSettings = {
+            "*".installation_mode = "blocked";
+            "uBlock0@raymondhill.net" = {
+               install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+               installation_mode = "force_installed";
+            };
+		 };
+      };
    };
    # ---
 
