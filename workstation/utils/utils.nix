1d8dd4daf816d4a568204a35c31aa23cfb940c0b
Kiana <luigiq99@gmail.com>
2025-10-07 17:39:55 +0200

    Update firefox extensions

--- workstation/utils/utils.nix
+++ workstation/utils/utils.nix
@@ -9,7 +9,6 @@
          languagePacks = ["en-US" "pl"];
          DisplayBookmarksToolbar = "always';
          ExtensionSettings = {
-            "*".installation_mode = "blocked";
             "uBlock0@raymondhill.net" = {
                install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
                installation_mode = "force_installed";
