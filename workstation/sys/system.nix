5b24fe4d35eaa5986cb83743f49d20ace2a5788e
Kiana <luigiq99@gmail.com>
2025-10-07 17:57:14 +0200

    Remove obbselete submodules

--- workstation/sys/system.nix
+++ workstation/sys/system.nix
@@ -221,10 +221,6 @@
          enable = true;
          qemu = {
             swtpm.enable = true;
-            ovmf = {
-               enable = true;
-               packages = [ pkgs.OVMFFull.fd ];
-			};
 		 };
 	  };
       spiceUSBRedirection.enable = true;
