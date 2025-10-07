1cc7f3ba58fd9dfd97bdb0cbc1056967f538c84e
Kiana <luigiq99@gmail.com>
2025-10-07 17:41:13 +0200

    Update utils.nix

--- workstation/utils/utils.nix
+++ workstation/utils/utils.nix
@@ -8,12 +8,6 @@
          enable = true;
          languagePacks = ["en-US" "pl"];
          DisplayBookmarksToolbar = "always';
-         ExtensionSettings = {
-            "uBlock0@raymondhill.net" = {
-               install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-               installation_mode = "force_installed";
-            };
-		 };
       };
    };
    # ---
