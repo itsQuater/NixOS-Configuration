02f35135fb09261476f262553df5a12403326912
Kiana <luigiq99@gmail.com>
2025-10-11 01:04:38 +0200

    Add OBS

--- workstation/utils/utils.nix
+++ workstation/utils/utils.nix
@@ -65,7 +65,27 @@
    };
    # ---
 
-   
+
+   # OBS audio/screen capture.
+   # ---
+   programs.obs-studio = {
+      enable = true;
+      package = (
+         pkgs.obs-studio.override {
+            cudaSupport = true;
+         }
+      );
+      plugins = with pkgs.obs-studio-plugins; [
+         wlrobs
+         obs-backgroundremoval
+         obs-pipewire-audio-capture
+         obs-gstreamer
+         obs-vkcapture
+      ];
+   };
+   # ---
+
+
    # Install other applications.
    # ---
    imports = [
